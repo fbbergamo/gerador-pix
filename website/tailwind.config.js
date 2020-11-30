6164600ad1e3fbe42f93eb94d281294429db89c4
Felipe Bergamo <fbbergamo@gmail.com>
2020-11-30 00:51:11 -0300

    webpack purger correct files

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: {
-    content: ["public/index.html", "underline", "text-sm", "inline-flex","items-center","px-4","py-2","border","border-transparent","text-base","leading-6","font-medium","rounded-md","text-white","bg-indigo-600","hover:bg-indigo-500","focus:outline-none","focus:border-indigo-700","focus:shadow-outline-indigo","active:bg-indigo-700","transition","ease-in-out","duration-150"],
+    content: ["../views/*", "underline", "text-sm", "inline-flex","items-center","px-4","py-2","border","border-transparent","text-base","leading-6","font-medium","rounded-md","text-white","bg-indigo-600","hover:bg-indigo-500","focus:outline-none","focus:border-indigo-700","focus:shadow-outline-indigo","active:bg-indigo-700","transition","ease-in-out","duration-150"],
     options: {
       whitelist: ['hidden'],
     }
