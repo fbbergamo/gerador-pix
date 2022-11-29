d1fdd0d1b13ea96c9b986e94bdcaba802726629c
Felipe Bergamo <fbbergamo@gmail.com>
2022-11-29 01:40:39 -0300

    add flex col to purge css allow list

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   purge: {
     content: ["../views/**/*", "underline", "flex-col", "text-sm", "inline-flex","items-center","px-4","py-2","border","border-transparent","text-base","leading-6","font-medium","rounded-md","text-white","bg-indigo-600","hover:bg-indigo-500","focus:outline-none","focus:border-indigo-700","focus:shadow-outline-indigo","active:bg-indigo-700","transition","ease-in-out","duration-150"],
     options: {
-      safelist: ['hidden'],
+      safelist: ['hidden', 'flex-col'],
     }
   },
   theme: {
